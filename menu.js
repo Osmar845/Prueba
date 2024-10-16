c2d794ac84af6eca19461f1a4981550c6bdeca52
Osmar Castañeda <115645141+Osmar845@users.noreply.github.com>
2024-10-15 19:19:56 -0600

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,4 +1,15 @@
-document.getElementById("menu-icon").addEventListener("click", function() {
-    var menu = document.getElementById("menu-list");
-    menu.classList.toggle("active");
+document.addEventListener('DOMContentLoaded', (event) => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.menu-item');
+
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+
+    // Cierra el menú si se hace clic fuera de él
+    document.addEventListener('click', (event) => {
+        if (!event.target.closest('.menu')) {
+            navLinks.classList.remove('active');
+        }
+    });
 });
